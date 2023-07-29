84b7c1f0764fc51a0337791c426526206ed35c89
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2023-07-29 14:22:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import matplotlib.pyplot as plt
 import implementation
-import os 
+import os
 
 app = Flask(__name__)
 app.secret_key = 'BAD_SECRET_KEY'
@@ -10,6 +10,7 @@ app.secret_key = 'BAD_SECRET_KEY'
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/process_video', methods=['POST'])
 def process_video():
@@ -20,18 +21,15 @@ def process_video():
     # Save video file to a temporary location
     video_path = 'static/' + id + '.webm'
     video.save(video_path)
-    print("saved video to:", video_path)
 
-
-    rPPG_filtered, rPPG_peaks = implementation.find_rPPG(video_path) 
+    rPPG_filtered, rPPG_peaks = implementation.find_rPPG(video_path)
     if rPPG_filtered is None:
         session['jumlah_peaks'] = 0
-        return {
+        return jsonify({
             'status': 'Tak De Mukee',
-            'code': 404,    
-            }
-    
-    print("rPPG_filtered:", rPPG_filtered)
+            'code': 404,
+        })
+
     # Plot the rPPG signal and peaks within the app context
     with app.app_context():
         plt.switch_backend('Agg')  # Use Agg backend for saving figures
@@ -39,7 +37,7 @@ def process_video():
         ax.plot(rPPG_filtered, label='rPPG Signal')
         if len(rPPG_peaks) > 0:
             ax.plot(rPPG_peaks, rPPG_filtered[rPPG_peaks], "x", label='Peaks')
-        
+
         img_path = 'static/gambar_' + id + '.png'
         fig.savefig(img_path)
         plt.close(fig)
@@ -52,13 +50,13 @@ def process_video():
     # remove video file
     if os.path.exists(video_path):
         os.remove(video_path)
-        
-    return {
-            'status': 'success',
-            'code': 200,
-            'jumlah_peaks': heart_rate,
-            'image': url_for('static', filename='gambar_' + id + '.png'), 
-            }
+
+    return jsonify({
+        'status': 'success',
+        'code': 200,
+        'jumlah_peaks': heart_rate,
+        'image': img_path,
+    })
 
 
 if __name__ == '__main__':
